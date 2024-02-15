f575c0a6fbb4bb16d03a8b7faeed843b558f9524
Sartek <andrey.o@48pacific.com>
2024-02-15 23:36:26 +0200

    api url fix

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -1,2 +1,2 @@
-const API_HOST = process.env.API_HOST || 'http://localhost:5000' || `https://calendar-app-h5p1.vercel.app`
+const API_HOST = process.env.API_HOST || `https://calendar-app-h5p1.vercel.app`
 export const API_URL = API_HOST + '/api/'
